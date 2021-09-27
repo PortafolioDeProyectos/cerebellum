4285ae09c99542f936bc978eebaba21d06ea6aee
Joaquin Guzman <joaquinguzman282@gmail.com>
2021-09-27 17:19:12 -0300

    muestra de productos sin css

---
+++ components/layout/DetallesProducto.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "@emotion/styled";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import { es } from "date-fns/locale";
+const Imagen = styled.img`
+  width: 200px;
+`;
+const DetallesProducto = ({ producto }) => {
+  const {
+    id,
+    comentarios,
+    creado,
+    descripcion,
+    empresa,
+    nombre,
+    url,
+    urlImagen,
+    votos,
+  } = producto;
+  return (
+    <li>
+      <div>
+        <div>
+          <Imagen src={urlImagen} />
+        </div>
+        <div>
+          <h1>{nombre}</h1>
+          <p>{descripcion}</p>
+          <img src="/static/img/comentario.png" />
+          <p>{comentarios.length} Comentarios</p>
+        </div>
+        <p>
+          Publicado hace :
+          {formatDistanceToNow(new Date(creado), { locale: es })}
+        </p>
+      </div>
+      <div>
+        <div>&#9650;</div>
+        <p>{votos}</p>
+      </div>
+    </li>
+  );
+};
+
+export default DetallesProducto;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React, { useState, useEffect, useContext } from "react";
 import Layout from "../components/layout/Layout";
+import { FirebaseContext } from "../firebase";
+
+import styled from "@emotion/styled";
 import { css } from "@emotion/react";
+import DetallesProducto from "../components/layout/DetallesProducto";
 
 const Parrafo = styled.p`
   max-width: 1200px;
@@ -9,9 +12,41 @@ const Parrafo = styled.p`
   margin: 2rem auto 0 auto;
 `;
 const Inicio = () => {
+  const [productos, setProductos] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        .onSnapshot(manejarSnapshot);
+    };
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    setProductos(productos);
+  }
   return (
     <div>
       <Layout>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-whiteS">
+              {productos.map((producto) => (
+                <DetallesProducto key={producto.id} producto={producto} />
+              ))}
+            </ul>
+          </div>
+        </div>
+
         <h1
           css={css`
             text-align: center;
